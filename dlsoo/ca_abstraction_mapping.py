824311d70f7731d11126ba7c49ac7d0d1e287b08
marlibgin <marco.apollonio@diamond.ac.uk>
2018-07-31 10:51:40 +0100

    error bars / boxes switched correctly in plot.py
    lt_proxy parameters now changeable/readable through gui / external file

--- dlsoo/ca_abstraction_mapping.py
+++ dlsoo/ca_abstraction_mapping.py
@@ -1,11 +1,20 @@
 from __future__ import division
 import math
-
+import numpy as np
 from cothread.catools import caget
 
 
 # Set before use in gui.py
 NUMBER_OF_BUNCHES = None
+
+
+
+def read_data(): 
+    
+    #data = np.loadtxt("/dls/physics/students/clp36697/dls_online_optimizer-plotting/results/lifetime_proxy_details")
+    data = np.loadtxt("./lifetime_proxy_details")
+    return data 
+          
 
 
 def bunch_length(I_beam):
@@ -14,13 +23,15 @@ def bunch_length(I_beam):
     return bunch_length_value
 
 
-def lifetime_proxy():
 
-    sy_ref = 12.2 # 8/5/2018 vertical beam size in um
+def lifetime_proxy():
+    data = read_data() 
+            
+    vert_beam_size = data[0]   # 12.2 # 8/5/2018 vertical beam size in um
     sy     = caget('SR-DI-EMIT-01:P1:SIGMAY_MEAN')
     I_beam = caget('SR-DI-DCCT-01:SIGNAL')
     PMT_count = caget('SR-DI-COUNT-01:MEAN')+0.001
-    objective =  PMT_count /  PMT_ref(I_beam) * sy_ref / sy  # rescaled n. of losses (note it was sy / sy_ref, corrected after IPAC)
+    objective =  PMT_count /  PMT_ref(I_beam) * vert_beam_size / sy  # rescaled n. of losses (note it was sy / sy_ref, corrected after IPAC)
     print('LT_proxy_resc='+str(objective))
     
 #    epsilon_y = caget('SR-DI-EMIT-01:VEMIT_MEAN')
@@ -37,7 +48,10 @@ def PMT_ref(x):  # MA 17/4/2018
 #    PMT_ref = -2.608e-6*x**4 + 0.000502*x**3 + 0.4294*x**2 + 18.55*x -258.2
 #old    PMT_ref =  6.3521e-6*x**4 - 0.0046326*x**3 +2.1405*x**2 -25.915*x +307.87
 #    PMT_ref = 1.3e-6*x**4 -0.0015*x**3 +0.68*x**2 +0.74*x +22
-    PMT_ref = 2.1e-6*x**4 -0.0024*x**3 + 0.81*x**2 -0.88*x -0.55
+
+    #PMT_ref = 2.1e-6*x**4 -0.0024*x**3 + 0.81*x**2 -0.88*x -0.55
+    data = read_data() 
+    PMT_ref = data[5]*x**4 + data[4]*x**3 + data[3]*x**2 + data[2]*x + data[1]    
     return PMT_ref
 
 
--- dlsoo/plot.py
+++ dlsoo/plot.py
@@ -5,20 +5,22 @@ Created on 19 Jul 2017
 
 @author: David Obee and James Rogers
 """
+
+
 from __future__ import division
 
 import operator
 
 import matplotlib.cm as cm
-import numpy
+import numpy 
 import matplotlib.pyplot as plt
 import matplotlib.patches as pat
 
+#------------------------------------------------------------ CORRECT PARETO PLOTTING FUNCTION ----------------------------------------------#
 
 def virtual_pareto_points(x_pareto, y_pareto, signConverter):
     """
-    The plotting needs to correctly show the correct dominated region.
-    This is done by creating artificial points:
+    The plotting needs to correctly show the correct dominated region. This is done by creating artificial points:
 
           ///////////
     y1 |  x-----o ///
@@ -29,9 +31,8 @@ def virtual_pareto_points(x_pareto, y_pareto, signConverter):
         ----------->
           x1    x2
 
-    for points on pareto front (x1,y1) and (x2,y2), an artificial point is
-    put at (x2,y1). This will change however if objectives are being
-    minimised/maximised (this is why signconverter is used).
+    for points on pareto front (x1,y1) and (x2,y2), an artificial point is put at (x2,y1). This will change however
+    if objectives are being minimised/maximised (this is why signconverter is used).
     """
 
     if signConverter == [1,1] or signConverter == [1,-1]:
@@ -70,6 +71,8 @@ def virtual_pareto_points(x_pareto, y_pareto, signConverter):
         new_y = [y[1] for y in new_coords]
 
         return new_x, new_y
+
+#--------------------------------------------------------- PLOTTING CLASSES FOR PROGRESS AND FINAL RESULTS ------------------------------------------#
 
 
 def plot_pareto_fronts(file_names, ax, axis_labels, signConverter):
@@ -120,14 +123,7 @@ def plot_pareto_fronts(file_names, ax, axis_labels, signConverter):
     ax.set_ylabel(axis_labels[1])
 
 
-def plot_pareto_fronts_interactive(file_names,
-                                   ax,
-                                   axis_labels,
-                                   interactor,
-                                   callback,
-                                   view_mode,
-                                   signConverter,
-                                   initial_measurements=None):
+def plot_pareto_fronts_interactive(file_names, ax, axis_labels, interactor, callback, view_mode, signConverter, initial_measurements=None):
     """
     This is used in the final results plot
     """
@@ -138,65 +134,101 @@ def plot_pareto_fronts_interactive(file_names,
 
     fs = []
 
-    # execute all FRONTS files to obtain the pareto fronts.
+    #execute all FRONTS files to obtain the pareto fronts.
     for file_name in file_names:
         execfile(file_name)
 
         fs.append(locals()['fronts'][0])
+    print "fs is: {}".format(fs) 
 
-    x_vals = []
-    y_vals = []
+    
+    #different view modes for the final plot
 
-    # different view modes for the final plot
-    if view_mode == 'No focus':
-        # Use evenly spaced values in the jet color map
+    if view_mode == "No focus":
+
         colors = cm.jet(numpy.linspace(0, 1, len(fs)))
 
         for nf, f in enumerate(fs):
+            x_vals = []
+            y_vals = []
+            std_Y = []
+            std_X = [] 
+            mean_errX = []                 
+            mean_errY = []    
+            STD_bar_X = []
+            STD_bar_Y = []
+            mean_bar_X = []
+            mean_bar_Y = []
 
             for ni, i in enumerate(f):
-
+               
                 #take into account the change of sign for max/min
                 x_vals.append(i[1][0]*signConverter[0])
                 y_vals.append(i[1][1]*signConverter[1])
 
-                #add standard error of mean
-                x_err = i[2][0]
-                y_err = i[2][1]
-
-
+                #add standard error of mean 
+                 
+                mean_errX.append(i[2][0]) 
+                mean_errY.append(i[2][1]) 
+		
+		
                 #diam_x = x_err * 2
                 #diam_y = y_err * 2
-
-                #standard_dev_err_X = i[3][0]
-                dev_errY = i[3][1]
-                dev_errX = i[3][0]
-
-
-                #fig, (ax0, ax1) = plt.subplots(nrows =2, sharex = True)
-                #ax0.errorbar(x_vals, y_vals, yerr= dev_errY, fmt='-o')
-                 #plt.show()
-
-
-
+		
+		#standard_std_X = i[3][0]
+                
+		std_Y.append(i[3][1])
+		std_X.append(i[3][0])
+                
                 if nf == len(fs) - 1:
-                    box = pat.Rectangle(xy=(i[1][0]*signConverter[0] - 0.5*x_err, i[1][1]*signConverter[1] - 0.5*y_err), width=x_err, height=y_err) # changed for standard error of mean boxes rather than ellipses rhs 16/07/18
-                    box.set_facecolor('none')
-                    ax.add_artist(box)
+                        #print "[X_val, Y_val]                          Standard Dev.                               error of mean"
+                        #print "##########################################################"      
+                        #print ni                   
+                        #print px_vals[ni], py_vals[ni], std_X[ni], std_Y[ni], x_err[ni], y_err[ni]
+                         
+		        STD_bar_X.append(std_X[ni])
+                        STD_bar_Y.append(std_Y[ni]) 
+                        mean_bar_X.append(mean_errX[ni])
+			mean_bar_Y.append(mean_errY[ni]) 
+		#fig, (ax0, ax1) = plt.subplots(nrows =2, sharex = True)
+		#ax0.errorbar(x_vals, y_vals, yerr= std_Y, fmt='-o')
+ 		#plt.show()
+                
+               
 
-
-
+                #if nf == len(fs) - 1:
+                    
+                    #box = pat.Rectangle(xy=(x_vals[ni] - bar_errorX2[ni], y_vals[ni] - bar_errorY2[ni]), width=2*bar_errorX2[ni], height=2*bar_errorY2[ni], color = 'red') # changed for standard error of mean boxes rather than ellipses rhs 16/07/18
+                  
+                    #box.set_facecolor('none')
+                    #ax.add_artist(box)
+		   
+	    
+            	   
             px_vals = [x for (x, y) in sorted(zip(x_vals, y_vals))]
             py_vals = [y for (x, y) in sorted(zip(x_vals, y_vals))]
-
-
+            #print "the x values plotted:" , px_vals         
+            
             #Plot the FINAL front in bold
             if nf == len(fs) - 1:
-                ax.errorbar(px_vals, py_vals, dev_errY, dev_errX, color= 'blue',ecolor= 'crimson', marker='o', picker=5, linestyle='None')  #bars not ellipses added rhs 16/07/18
-
+		
+		ax.errorbar(px_vals, py_vals, STD_bar_Y, STD_bar_X, color= 'blue',ecolor= 'crimson', marker='o', picker=5, linestyle='None')  #bars not ellipses added rhs 16/07/18
+		 
+		#for i in range(len(bar_errorY2)):
+		    #box = pat.Rectangle(xy=(px_vals[i] - bar_errorX2[i], py_vals[i] - bar_errorY2[i]), width=2*bar_errorX2[i], height=2*bar_errorY2[i]) # changed for standard error of mean boxes rather than ellipses rhs 16/07/18
+                    #box.set_facecolor('none')
+                    #ax.add_artist(box)
+		for i in range(len(STD_bar_Y)):
+		    box = pat.Rectangle(xy=(px_vals[i] - mean_bar_X[i], py_vals[i] - mean_bar_Y[i]), width=2*mean_bar_X[i], height=2*mean_bar_Y[i]) # changed for standard error of mean boxes rather than ellipses rhs 16/07/18
+                    box.set_facecolor('none')
+                    ax.add_artist(box)
+		      
+                    
+			
                 new_x, new_y = virtual_pareto_points(px_vals,py_vals,signConverter)
+		
                 ax.plot(new_x, new_y, color=colors[nf], linewidth=2)
-
+			
             #Plot the past fronts normally
             else:
                 ax.plot(px_vals, py_vals, color=colors[nf], marker='.', linestyle='None')
@@ -229,67 +261,50 @@ def plot_pareto_fronts_interactive(file_names,
                 x_err = i[2][0]
                 y_err = i[2][1]
 
-
-                #standard_dev_err_X = i[3][0] -rhs
-                dev_errY = i[3][1]
-                dev_errX = i[3][0]
-
-                #if nf == len(fs) - 1:
-                    #ell = pat.Ellipse(xy=(i[1][0]*signConverter[0], i[1][1]*signConverter[1]), width=x_err, height=y_err)
-                    #ell.set_facecolor('none')
-                    #ell.set_edgecolor('white')
-                    #ax.add_artist(ell)
-
-
-                # changed for standard error of mean boxes rather than ellipses rhs 16/07/18
                 if nf == len(fs) - 1:
-                    box = pat.Rectangle(xy=(i[1][0]*signConverter[0] - 0.5*x_err, i[1][1]*signConverter[1] - 0.5*y_err), width=x_err, height=y_err)
-                    box.set_facecolor('none')
-                    ax.add_artist(box)
+                    ell = pat.Ellipse(xy=(i[1][0]*signConverter[0], i[1][1]*signConverter[1]), width=x_err, height=y_err)
+                    ell.set_facecolor('none')
+                    ell.set_edgecolor('white')
+                    ax.add_artist(ell)
+
 
             px_vals = [x for (x, y) in sorted(zip(x_vals, y_vals))]
             py_vals = [y for (x, y) in sorted(zip(x_vals, y_vals))]
 
-            # add error ellipses
-            x_err = i[2][0]
-            y_err = i[2][1]
-            # diam_x,y substitutes err_x,y
-            diam_x = x_err * 2
-            diam_y = y_err * 2
 
             if nf == len(fs) - 1:
-                #ax.plot(px_vals, py_vals, color='y', marker='D', linestyle='None', picker=5)
-                ax.errorbar(px_vals, py_vals, dev_errX, dev_errY, color= 'blue',ecolor= 'crimson', marker='o', picker=5, linestyle='None')  #bars not ellipses added rhs 16/07/18
+                ax.plot(px_vals, py_vals, color='y', marker='D', linestyle='None', picker=5)
+
                 new_x, new_y = virtual_pareto_points(px_vals,py_vals,signConverter)
                 ax.plot(new_x, new_y, color='y', linewidth=2)
             else:
                 ax.plot(px_vals, py_vals, color="{0}".format(greys[nf]), marker='.', linestyle='None')
 
-        if nf == len(fs) - 1:  # Plot the FINAL front in bold
-            ax.plot(px_vals, py_vals, color=colors[nf], marker='D', picker=5, linestyle='None')
-            new_x, new_y = virtual_pareto_points(px_vals,py_vals,signConverter)
-            ax.plot(new_x, new_y, color=colors[nf], linewidth=2)
-        else:  # Plot the past fronts normally
-            ax.plot(px_vals, py_vals, color=colors[nf], marker='.', linestyle='None')
-            new_x, new_y = virtual_pareto_points(px_vals,py_vals,signConverter)
-            ax.plot(new_x, new_y, color=colors[nf], linestyle='--')
+                new_x, new_y = virtual_pareto_points(px_vals,py_vals,signConverter)
+                ax.plot(new_x, new_y, color="{0}".format(greys[nf]), linestyle='--')
 
-        x_vals = []
-        y_vals = []
+            x_vals = []
+            y_vals = []
 
-    if initial_measurements is not None:
-        # now plot the initial config
-        ax.plot(initial_config[0], initial_config[1], marker='o', markersize=10)
-    ax.grid()
+        if initial_measurements is not None:
+            # now plot the initial config
+            ax.plot(initial_config[0], initial_config[1], marker='o', markersize=10)
+        ax.grid()
 
     ax.set_xlabel(axis_labels[0])
     ax.set_ylabel(axis_labels[1])
 
 
+
+
+
 def plot_strip_tool(ax, data_sets, data_times):
     """
-    This is the plotting for the Striptool, which is turned off by default
-    in the main GUI.
+    This is the plotting for the Striptool, which is tunred off by default in the main GUI
     """
+
     data_set = [i[0] for i in data_sets]
+
     ax.plot(data_times, data_set)
+
+
